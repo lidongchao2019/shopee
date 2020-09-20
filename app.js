2691c3fabd251b345de069da339332b414a8fc48
lidongchao <ldc@itcast.cn>
2020-09-20 10:06:36 +0800

    'v1.0'

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+
 const memberRouter = require('./router/member');
 app.use('/api', memberRouter);
 
@@ -10,6 +15,6 @@ app.use('/api', goodsRouter);
 const cartRouter = require('./router/cart');
 app.use('/api', cartRouter);
 
-app.listen(80, (req, res) => {
-    console.log('server starting at 80...');
+app.listen(80, function() {
+    console.log('server running at http://localhost:80...');
 });
